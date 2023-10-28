749c2f3b9e22289f02959c9c1804529e2cd9a2a1
ANKIT GUPTA <ankitgupta1181@gmail.com>
2023-10-29 01:24:10 +0530

    Initial commit

--- src/app/MYComponents/todos/todos.component.ts
+++ src/app/MYComponents/todos/todos.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
 
+export class TodosComponent implements OnInit {
+
+  todos: Todo[] ;
+
+  constructor() { 
+
+    this.todos =[
+      {
+        sno:1,
+        title:"This is title",
+        desc: "Description",
+        active:true
+      },
+      {
+        sno:2,
+        title:"This is title2",
+        desc: "Description",
+        active:true
+      },
+      {
+        sno:3,
+        title:"This is title3",
+        desc: "Description",
+        active:true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
